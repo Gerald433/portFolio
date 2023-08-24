8039692afc8107ed42b3cc211293259a6cc96ac1
Gérald Gaillard <gerald8142@gmail.com>
2023-08-24 12:51:46 +0200

    Pass a props href to the linkfooter component

--- src/components/LinkFooter.js
+++ src/components/LinkFooter.js
@@ -1,12 +1,17 @@
 import styles from "./LinkFooter.module.scss";
 
-function LinkFooter({src}) {
+function LinkFooter({ src, href }) {
   return (
     <div
-      className={`${styles.link} d-flex align-items-center justify-content-center`}
-    >
-      <img src={src} alt="Logo du lien" />
+      className={`${styles.link} d-flex align-items-center justify-content-center`}>
+      <a href={href}>
+        <img src={src} alt="Logo du lien" />
+      </a>
     </div>
+
+
+
+        
   );
 }
 export default LinkFooter;
