8af7983492f3c5e349df01ddc68fe41988a917fa
Gérald Gaillard <gerald8142@gmail.com>
2023-08-24 12:45:22 +0200

    Build all the pages

---
+++ src/pages/ProjetsList.js
@@ -0,0 +1,20 @@
+import Header from'../components/Header';
+import Footer from'../components/Footer';
+
+const ProjetsList = function(){
+    return(
+    <>
+    <Header />
+    <Footer />
+    </>
+        
+
+
+
+
+
+
+    )
+}
+
+export default ProjetsList;
