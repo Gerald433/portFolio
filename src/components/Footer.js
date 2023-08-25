a120b2ac0ab3bdfc8b204eab22d9df4bbfd68db1
Gérald Gaillard <gerald8142@gmail.com>
2023-08-25 10:12:17 +0200

    Rework the footer with flexbox and links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,35 +3,51 @@ import linkLetter from "../assets/images/linkLetter.svg";
 import LinkFooter from "./LinkFooter";
 import linkLinkedin from "../assets/images/linkLinkedin.svg";
 import linkGithub from "../assets/images/linkGithub.svg";
+import { Link } from "react-router-dom";
 
 function Footer() {
   return (
     <footer className={`${styles.footer}`}>
-      <span className={`${styles.icon}`}>gg.</span>
-      <ul className={`${styles.connections} d-flex flex-column`}>
-        <li>Réseaux</li>
-        <li>
-          <a href={"https://github.com/Gerald433"}>Github</a>
-        </li>
-        <li>
-          <a href="https://www.linkedin.com/in/g%C3%A9rald-gaillard-218664277/">
-            Linkedin
-          </a>
-        </li>
-        <li>CV</li>
-        <li>Contact</li>
-        <li>Message</li>
-      </ul>
+      <div className={`${styles.container} container d-flex`}>
+        <span className={`${styles.icon}`}>gg.</span>
 
-      <div className={`${styles.containerLinkFoot} d-flex`}>
-        <LinkFooter
-          src={linkLinkedin}
-          href={"https://www.linkedin.com/in/g%C3%A9rald-gaillard-218664277/"}
-        />
-        <LinkFooter src={linkGithub} href={"https://github.com/Gerald433"} />
-        <LinkFooter src={linkLetter} />
+        <div>
+          <ul>
+            <p className={`${styles.titleLink}`}>Réseaux</p>
+            <li>
+              <Link to="https://github.com/Gerald433">Github</Link>
+            </li>
+            <li>
+              <Link to="https://www.linkedin.com/in/g%C3%A9rald-gaillard-218664277/">
+                Linkedin
+              </Link>
+            </li>
+            <li>
+              <Link to="#">CV</Link>
+            </li>
+          </ul>
+        </div>
+
+        <div>
+          <ul>
+            <p className={`${styles.titleLink}`}>Contact</p>
+            <li>
+              <Link to="#">Message</Link>
+            </li>
+          </ul>
+        </div>
+
+        
+
+        <div className={`${styles.containerLinkFoot} d-flex`}>
+          <LinkFooter
+            src={linkLinkedin}
+            href={"https://www.linkedin.com/in/g%C3%A9rald-gaillard-218664277/"}
+          />
+          <LinkFooter src={linkGithub} href={"https://github.com/Gerald433"} />
+          <LinkFooter src={linkLetter} />
+        </div>
       </div>
-
       <p className={`${styles.copyrigth}`}>
         -Port Folio Développeur Web Gérald Gaillard-
       </p>
--- src/components/LinkFooter.js
+++ src/components/LinkFooter.js
@@ -3,15 +3,12 @@ import styles from "./LinkFooter.module.scss";
 function LinkFooter({ src, href }) {
   return (
     <div
-      className={`${styles.link} d-flex align-items-center justify-content-center`}>
+      className={`${styles.link} d-flex align-items-center justify-content-center`}
+    >
       <a href={href}>
         <img src={src} alt="Logo du lien" />
       </a>
     </div>
-
-
-
-        
   );
 }
 export default LinkFooter;
