e58b0b0970612f885f726886ccf8bc93edea2dbf
Gérald Gaillard <gerald8142@gmail.com>
2023-08-25 13:29:52 +0200

    Restructure the header and make it responsive

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,25 +1,30 @@
 import styles from "./Header.module.scss";
 import { Link } from "react-router-dom";
-// import logo from "../assets/images/logo.jpg";
+import menuHeader from "../assets/images/menuHeader.svg";
 
 function Header() {
   return (
-    <header className={`${styles.header} d-flex `}>
-      <div className="d-flex flex-fill ">
-        <span className={`${styles.icon} d-flex align-items-center`}>gg.</span>
-        <ul className="d-flex align-items-center">
-          <li>
-            <Link to="/">Projets</Link>
-          </li>
-          <li>
-            <Link to="/a-propos">À propos</Link>
-          </li>
-        </ul>
+    <header className={`${styles.header}`}>
+      <div className="container d-flex">
+        <div className="d-flex flex-fill ">
+          <span className={`${styles.icon} d-flex align-items-center`}>
+            gg.
+          </span>
+          <ul className="d-flex align-items-center ">
+            <li>
+              <Link to="/">Projets</Link>
+            </li>
+            <li>
+              <Link to="/a-propos">À propos</Link>
+            </li>
+          </ul>
+        </div>
+        <div className={`${styles.contactBox} d-flex align-items-center `}>
+          <span className={`${styles.contact} d-flex align-items-center `}>
+            <Link to="/contact">Contact</Link>
+          </span>
+        </div>
       </div>
-
-      <span className={`${styles.contact} d-flex align-items-center`}>
-        <Link to="/contact">Contact</Link>
-      </span>
     </header>
   );
 }
