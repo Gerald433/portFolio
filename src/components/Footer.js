1f172766bcb66f64463d933b9655ea1ddf395ea9
Gérald Gaillard <gerald8142@gmail.com>
2023-08-29 06:33:47 +0200

    Apply style to footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -10,34 +10,35 @@ function Footer() {
     <footer className={`${styles.footer}`}>
       <div className={`${styles.container} container d-flex`}>
         <span className={`${styles.icon}`}>gg.</span>
+        <div className={`${styles.linkText} d-flex `}>
+          <div>
+            <ul>
+              <p className={`${styles.titleLink}`}>Réseaux</p>
+              <li>
+                <Link to="https://github.com/Gerald433">Github</Link>
+              </li>
+              <li>
+                <Link to="https://www.linkedin.com/in/g%C3%A9rald-gaillard-218664277/">
+                  Linkedin
+                </Link>
+              </li>
+              <li>
+                <Link to="#">CV</Link>
+              </li>
+            </ul>
+          </div>
 
-        <div>
-          <ul>
-            <p className={`${styles.titleLink}`}>Réseaux</p>
-            <li>
-              <Link to="https://github.com/Gerald433">Github</Link>
-            </li>
-            <li>
-              <Link to="https://www.linkedin.com/in/g%C3%A9rald-gaillard-218664277/">
-                Linkedin
-              </Link>
-            </li>
-            <li>
-              <Link to="#">CV</Link>
-            </li>
-          </ul>
+          <div>
+            <ul>
+              <p className={`${styles.titleLink}`}>Contact</p>
+              <li>
+                <Link to="#">Message</Link>
+              </li>
+            </ul>
+          </div>
         </div>
 
-        <div>
-          <ul>
-            <p className={`${styles.titleLink}`}>Contact</p>
-            <li>
-              <Link to="#">Message</Link>
-            </li>
-          </ul>
-        </div>
-
-        <div className={`${styles.containerLinkFoot} d-flex`}>
+        <div className={`${styles.containerLinkFoot} d-flex `}>
           <LinkFooter
             src={linkLinkedin}
             href={"https://www.linkedin.com/in/g%C3%A9rald-gaillard-218664277/"}
