09359f4d48cabea249d339e5ac5eb51f29fc720c
Gérald Gaillard <gerald8142@gmail.com>
2023-09-01 05:12:23 +0200

    Create the ProjectBox component

---
+++ src/components/projectBox.js
@@ -0,0 +1,26 @@
+import styles from "./projectBox.module.scss";
+
+function ProjectBox({ src, className }) {
+  return (
+    <>
+      <div
+        className={`${styles.box} ${className} d-flex flex-column  align-items-center`}
+      >
+        <span className={`${styles.formation}`}>FORMATION</span>
+        <h2
+          className={`${styles.title} d-flex flex-column justify-content-center`}
+        >
+          Booky
+        </h2>
+        <span className={`${styles.knowMore} `}>En savoir plus</span>
+        <img
+          className={`${styles.phoneBlack} `}
+          src={src}
+          alt="dessin d'un téléphone noir"
+        />
+      </div>
+    </>
+  );
+}
+
+export default ProjectBox;
