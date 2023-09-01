5d28ffa883128f765aec2ef55d7fde19d72080ad
Gérald Gaillard <gerald8142@gmail.com>
2023-09-01 08:56:58 +0200

    Set up aPropos page

--- src/pages/Apropos/Apropos.js
+++ src/pages/Apropos/Apropos.js
@@ -1,12 +1,31 @@
-function Apropos(){
-return(
-<>
-<h1>Ici c'est la page A propos</h1>
-</>
+import styles from "./Apropos.module.scss";
+import avatar from "../../assets/images/Avatar.svg";
 
-
-
-)
+function Apropos() {
+  return (
+    <>
+      <div className={`${styles.top} container d-flex flex-column `}>
+        <h1 className={`${styles.titlePrincipal} `}>I'm gérald g.</h1>
+        <div className={`${styles.blocPrincipal} container d-flex  `}>
+          <img src={avatar} alt="Ma présentation" />
+          <div className={`${styles.blocText} container d-flex flex-column `}>
+            <span className={`${styles.job} br`}>
+              Développeur Web Front-end,
+            </span>
+            <span className={`${styles.specialisation} br`}>
+              JavaScript - React.
+            </span>
+            <p className={`${styles.indications} `}>
+              Je crée des solutions web fonctionnelles et engageantes, donnant
+              vie aux idées numériques.<br/><br/><br/> Mon objectif est de répondre aux besoins
+              tout en garantissant une expérience utilisateur fluide et
+              captivante.
+            </p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default Apropos;
