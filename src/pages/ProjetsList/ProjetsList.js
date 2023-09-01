1f183c6ce8159c9cd2676c883f5e7d8909dcc525
Gérald Gaillard <gerald8142@gmail.com>
2023-09-01 05:11:33 +0200

    Create the ProjectsList component

--- src/pages/ProjetsList/ProjetsList.js
+++ src/pages/ProjetsList/ProjetsList.js
@@ -1,11 +1,60 @@
-import Header from "../../components/Header";
-import Footer from "../../components/Footer";
+import styles from "./ProjetsList.module.scss";
+import React from "react";
+import ProjectBox from "../../components/projectBox";
+import blackPhone from "../../assets/images/black-phone.svg";
+import whitePhone from "../../assets/images/white-phone.svg";
+import blackTablet from "../../assets/images/black-tablette.svg";
+import whiteTablet from "../../assets/images/white-tablette.svg";
 
 const ProjetsList = function () {
   return (
     <>
-    <h1>Bienvenue sur la page ProjetList</h1>
-    
+      <div className={`${styles.top} container d-flex flex-column br`}>
+        <h1 className={`${styles.titlePrincipal} br`}>I'm gérald g.</h1>
+        <span className={`${styles.job} br`}>Développeur Web Front-end,</span>
+        <span className={`${styles.specialisation} br`}>
+          JavaScript - React.
+        </span>
+        <p className={`${styles.indications} br `}>
+          Explorez ci-dessous quelques exemples de mes réalisations, illustrant
+          mon intéret pour le développement de solutions digitale pertinantes
+        </p>
+      </div>
+
+      <div
+        className={`${styles.presentationInFour} container d-flex justify-content-center align-items-center br`}
+      >
+        <ProjectBox src={whitePhone} className={`${styles.ligth}`} />
+
+        <ProjectBox src={blackTablet} className={styles.dark} />
+
+        <ProjectBox src={whiteTablet} className={styles.ligth} />
+
+        <ProjectBox src={blackPhone} className={styles.dark} />
+      </div>
+
+      <div className={`${styles.middle} container d-flex flex-column br`}>
+        <h2 className={`${styles.middleTitle}`}>En cours</h2>
+        <span className={`${styles.middleIndications} `}>
+          Découvrez mes projets de développement web en cours,
+        </span>
+      </div>
+
+      <div
+        className={`${styles.presentationInFour} container d-flex justify-content-center align-items-center br`}
+      >
+        <ProjectBox src={whitePhone} className={`${styles.ligth}`} />
+
+        <ProjectBox src={blackTablet} className={styles.dark} />
+      </div>
+
+      <p className={`${styles.finalParagraph} container d-flex flex-column br`}>
+        Ces projets ne sont que quelques-uns parmi les plus significatifs que
+        j'ai réalisés. Mon portfolio complet est disponible sur mon profil
+        GitHub, où vous pouvez explorer une variété de réalisations techniques.
+        N'hésitez pas à jeter un coup d'œil à l'ensemble de mon travail et à mes
+        contributions.
+      </p>
     </>
   );
 };
