68dd11bddd04a67dd332ba5da1f892b47203b4c0
Gérald Gaillard <gerald8142@gmail.com>
2023-09-13 15:05:52 +0200

    Make projectBox elements more dynamic

--- src/components/projectBox.js
+++ src/components/projectBox.js
@@ -1,24 +1,20 @@
 import styles from "./projectBox.module.scss";
-
-function ProjectBox({ src, className, title, description }) {
+import { Link } from "react-router-dom";
+function ProjectBox({ src, className, title, description, textAlt, type, projetId }) {
   return (
     <>
-      <div
-        className={`${styles.box} ${className} d-flex flex-column  align-items-center`}
+      <Link
+       to={`/projets/${projetId}`} className={`${styles.box} ${className} d-flex flex-column  align-items-center`}
       >
-        <span className={`${styles.formation}`}>FORMATION</span>
+        <span className={`${styles.formation}`}>{type}</span>
         <h2
           className={`${styles.title} d-flex flex-column justify-content-center`}
         >
           {title}
         </h2>
         <span className={`${styles.knowMore} `}>En savoir plus</span>
-        <img
-          className={`${styles.phoneBlack} `}
-          src={src}
-          alt="dessin d'un téléphone noir"
-        />
-      </div>
+        <img className={`${styles.phoneBlack} `} src={src} alt={textAlt} />
+      </Link>
     </>
   );
 }
