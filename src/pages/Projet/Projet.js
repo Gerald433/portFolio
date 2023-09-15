34867a6632fbbc0aa2d2f6f5d838f2880c07f742
Gérald Gaillard <gerald8142@gmail.com>
2023-09-15 09:12:01 +0200

    Improve the projet file

--- src/pages/Projet/Projet.js
+++ src/pages/Projet/Projet.js
@@ -4,6 +4,9 @@ import { useParams } from "react-router-dom";
 import projets from "../../assets/data/projets.json";
 import styles from "./Projet.module.scss";
 import Item from "../../components/Item";
+import logoListItem from "../../assets/images/itemList.svg";
+import hat from "../../assets/images/hat.svg";
+import hat2 from "../../assets/images/hat2.svg";
 
 function Projet() {
   const { projetId } = useParams();
@@ -31,8 +34,13 @@ function Projet() {
       <div className={` container d-flex flex-column `}>
         <section>
           <h1 className={`${styles.titleProjet}`}>{projet.title}</h1>
+          <div className={`${styles.boxLogos} d-flex `}>
+            {projet.technologies.map((techno, index) => (
+              <img key={index} src={techno.logo} alt="gtrg" />
+            ))}
+          </div>
 
-          <div className={`${styles.itemList} d-flex flex-column`}>
+          <div className={`${styles.itemList} d-flex flex-column `}>
             {projet.technologies.map((tech, index) => (
               <Item
                 key={index}
@@ -40,7 +48,9 @@ function Projet() {
                   index % 2 === 0 ? styles.small : styles.big
                 } d-flex `}
               >
-                <div>
+                <div
+                  className={`${styles.logoItemBox} d-flex align-items-center`}
+                >
                   <img
                     className={`${styles.logo}`}
                     src={tech.logo}
@@ -54,21 +64,50 @@ function Projet() {
             ))}
           </div>
 
-          <div  className={` container d-flex`}>
-            <img src={projet.pictures} alt="projet" />
-            
-            <ul  className={`${styles.specificationList}`}>
-            {projet.specifications.map((specific, index) =>
-             <li
-             key={index}>
-              {specific}
-             </li>
-            
-            )}
-            </ul>)
+          <div className={`${styles.middleBox} container d-flex `}>
+            <div className={`${styles.boxPicture1} `}>
+              <img className={`${styles.hat}`} src={hat} alt="entete" />
+              <img
+                className={`${styles.firstImg}`}
+                src={projet.pictures1}
+                alt="projet 1"
+              />
+            </div>
+            <ul className={`${styles.specificationList}`}>
+              {projet.specifications.map((specific, index) => (
+                <li key={index}>
+                  <img src={logoListItem} alt="logo de list" />
+                  {specific}
+                </li>
+              ))}
+            </ul>
+            )
           </div>
 
-          <p className={`${styles.description}`}>{projet.description}</p>
+          <p className={`${styles.description} `}>{projet.description}</p>
+          <div className={`${styles.boxPicture2} `}>
+            <img className={`${styles.hat2}`} src={hat2} alt="entete" />
+            <img
+              className={`${styles.secondImg}`}
+              src={projet.pictures2}
+              alt="projet 2"
+            />
+          </div>
+        </section>
+
+        <section className={`d-flex flex-column `}>
+          <h2 className={`${styles.titleSection}`}>Section 2</h2>
+
+          <div className={`${styles.boxEvaluation} `}>
+            <span className={`${styles.evaluator}`}>Evaluateur(trice)s</span>
+            <span className={`${styles.evaluatorJob}`}>
+              Développeur web FullStack
+            </span>
+            <span className={`${styles.evaluatorDate}`}>
+              le 10 Janvier 2022
+            </span>
+            <p className={`${styles.evaluation} `}>{projet.evaluation}</p>
+          </div>
         </section>
 
         <Link to="/">liste de projets</Link>
