f73973edafd7f3840f49cd6df3e1203f472bfdcc
Gérald Gaillard <gerald8142@gmail.com>
2023-10-05 10:32:10 +0200

    Install the yup and formik libraries

--- src/pages/Contact/Contact.js
+++ src/pages/Contact/Contact.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { useFormik } from "formik";
+import * as Yup from "yup"
 import styles from "./Contact.module.scss";
 import vector from "../../assets/images/Vector.svg";
 
