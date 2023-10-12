a3de44180fcb71dd690c8036385232a44eecfd11
Gérald Gaillard <gerald8142@gmail.com>
2023-10-12 18:05:03 +0200

    Add base name on BrowserRouter

--- src/index.js
+++ src/index.js
@@ -14,10 +14,9 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { routes } from "./routes";
 
-
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL || ""}>
     <ScrollToTop />
     <Header />
     <main>
