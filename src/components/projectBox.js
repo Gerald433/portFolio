7fd8658a6a9a026bb17979f09532d53042b3609b
Gérald Gaillard <gerald8142@gmail.com>
2023-10-19 11:08:31 +0200

    Redirect the user to another tab when clicking on a project

--- src/components/projectBox.js
+++ src/components/projectBox.js
@@ -9,11 +9,18 @@ function ProjectBox({
   type,
   projetId,
 }) {
+  const openProjectInNewTab = () => {
+    const newWindow = window.open(`/projets/${projetId}`);
+    newWindow.document.title = title;
+  };
+
   return (
     <>
       <Link
         to={`/projets/${projetId}`}
         className={`${styles.box} ${className} d-flex flex-column  align-items-center`}
+        target="_blank"
+        onClick={openProjectInNewTab}
       >
         <span className={`${styles.formation}`}>{type}</span>
         <h2
